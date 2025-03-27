88e2ff9a093342dd2cf8581df54be828a07191d9
arondesu <vallegaaron13@gmail.com>
2025-03-27 22:05:30 +0800

    instruction

--- DATABASE/DBFunc.cs
+++ DATABASE/DBFunc.cs
@@ -2,6 +2,8 @@ using MySql.Data.MySqlClient;
 
 public class DBFunc {
     private DBConn conn = new DBConn();
-
+    
+    //here e code ang function for database
+    //try{}catch ato gamiton to check
     
 }
