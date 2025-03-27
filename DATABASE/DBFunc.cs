8d693c40cf396ed6e22cb6b6dfd30cf2980e1ff9
arondesu <vallegaaron13@gmail.com>
2025-03-27 19:57:27 +0800

    remove Func

--- DATABASE/DBFunc.cs
+++ DATABASE/DBFunc.cs
@@ -3,14 +3,5 @@ using MySql.Data.MySqlClient;
 public class DBFunc {
     private DBConn conn = new DBConn();
 
-    public bool additem() {
-
-        try{
-            using MySqlConnection connection = conn.connectSql();
-
-            connection.Open();
-
-            string query = "INSERT INTO item (name) VALUES ('value1')";
-        }
-    }
+    
 }
