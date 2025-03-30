73bf9e3c7f348f10d9b92cea6021af2b473170ab
arondesu <vallegaaron13@gmail.com>
2025-03-30 16:23:36 +0800

    byebye

--- DATABASE/DBFunc.cs
+++ DATABASE/DBFunc.cs
@@ -1,7 +1,8 @@
 using MySql.Data.MySqlClient;
 
 public class DBFunc {
-    private DBConn conn = new DBConn();
+
+    //private DBConn conn = new DBConn();
     
     //here e code ang function for database
     //try{}catch ato gamiton to check
