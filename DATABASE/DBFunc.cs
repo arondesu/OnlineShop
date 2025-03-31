470ad6837453a65ac6f9a25c8a91c104fc35c198
arondesu <vallegaaron13@gmail.com>
2025-03-31 19:28:49 +0800

    added: connection file function file code removed query from login btn
    to be check if mo konek

--- DATABASE/DBFunc.cs
+++ DATABASE/DBFunc.cs
@@ -1,16 +1,45 @@
+using System.Data.SqlClient;
+using System.Data;
 using MySql.Data.MySqlClient;
+using OnlineShop;
+using OnlineShop.DATABASE;
 
-public class DBFunc {
+public class DBFunc
+{
 
-    //private DBConn conn = new DBConn();
-    
-    //here e code ang function for database
-    //try{}catch ato gamiton to check
+    private DBConn dbConn = new DBConn();
 
-    
-//testo testo
+    public bool isLoginTrue(string username, string password)
+    {
+        try
+        {
+            using MySqlConnection conn = dbConn.GetConnection();
 
-    //test DB again
-    
+            conn.Open();
 
+            string query = "SELECT * FROM Login WHERE Username = @username AND Password = @password";
+
+            using MySqlCommand command = new MySqlCommand(query, conn);
+            command.Parameters.AddWithValue("@username", username);
+            command.Parameters.AddWithValue("@password", password);
+
+            int count = Convert.ToInt32(command.ExecuteScalar());
+
+            if (count > 0)
+            {
+                MessageBox.Show("Login Successful!");
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show("Error: " + e.Message);
+            return false;
+        }
+    }
 }
