c06181d4985763ea208e7a68b64f394513b7e707
Karl Martin Gaviola <Wootskarl@outlook.sg>
2025-04-27 21:40:24 +0800

    Dont mind this just saving the changes

--- obj/Debug/net8.0-windows/OnlineShop.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/OnlineShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e11586d740c58fe79b587e6cd10b30201f6fe0aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8b106c9e99e30163e52ea8e00001ce6cf967f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
