5b2783da1045db2a30c75731c2c13414d62c41fe
arondesu <vallegaaron13@gmail.com>
2025-04-29 17:29:35 +0800

    #

--- obj/Debug/net8.0-windows/OnlineShop.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/OnlineShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3769b11b3e739ce3c9a18a350b62763f379f0803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56087507832beaed671a1f4091c70a7e07a5ea62")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
