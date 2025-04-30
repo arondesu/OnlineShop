48c2a91a0a0dfc87d2eed095fa03088578e0a230
Karl Martin Gaviola <Wootskarl@outlook.sg>
2025-04-30 09:03:51 +0800

    #

--- obj/Debug/net8.0-windows/OnlineShop.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/OnlineShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00efa205be96903c1c2e125979a682837f3104d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25fa722710436b3bd0f842cb65681fa6ebe61579")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
